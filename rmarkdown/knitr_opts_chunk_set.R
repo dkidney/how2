e1bfc54cae18c1ade987d3d11d4860ca2230d7eb
darrenkidney <darren.kidney@nielsen.com>
2019-06-05 22:23:59 +0100

    updated rmarkdown

--- rmarkdown/knitr_opts_chunk_set.R
+++ rmarkdown/knitr_opts_chunk_set.R
@@ -26,7 +26,7 @@ knitr::opts_chunk$set(
         family = "Avenir",
         pointsize = 11
     ),
-    dpi = 100, # dpi * inches = pixels
+    dpi = 96, # dpi * inches = pixels
     fig.width = 7,
     # fig.height = 7 / golden_ratio,
     fig.asp = 1 / golden_ratio, # fig.height = fig.width * fig.asp
@@ -42,14 +42,16 @@ ggplot2::theme_set(
         base_line_size = 0.5
     )
 )
-# python
+# engines
 knitr::opts_chunk$set(
     engine.path = list(
-        python = "/usr/local/Cellar/python/3.7.3/bin/python3"
+        python = "/usr/local/Cellar/python/3.7.3/bin/python3",
+        sh = ,
+        zsh = 
     )
 )
 stopifnot(file.exists(knitr::opts_chunk$get("engine.path")$python))
-# reticulate
+# python - requires reticulate
 knitr::opts_chunk$set(
     python.reticulate = TRUE
 )
@@ -66,10 +68,18 @@ knitr::opts_chunk$set(
 )
 # option templates
 knitr::opts_template$set(
-    opts.label = list() # see ?opts_template
+    cat_file = list(eval = TRUE, echo = FALSE, results = "asis")
 )
+cat_file = function(x){
+    y = readLines(x, warn = FALSE) %>% paste0(collapse = "\n")
+    paste0("```", tools::file_ext(x), "\n", y, "\n```")
+}
 # package options - see ?opts_knit
 knitr::opts_knit$set(
     aliases = c(h = 'fig.height', w = 'fig.width'),
-    width = 100
+    width = 96
 )
+# working directory
+if (interactive()) {
+    setwd(fs::path_dir(rstudioapi::getActiveDocumentContext()$path))
+}
