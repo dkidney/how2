cb5e04a02a683bc715b8dfe6d0c37224f9201089
dkidney <darrenkidney@yahoo.co.uk>
2019-07-07 22:25:00 +0100

    wip r clean up

--- wip_r/Rprofile - how to find it.R
+++
@@ -1,9 +0,0 @@
-################################################################################
-## ON A MAC
-
-# Mine was automatically saved at the highest level on the hard drive when R was installed and appeared in the Files tab in 'RStudio'
-# I tried moving it to the 'Documents' folder but when I did that it didn't work so I moved it back to the original location
-
-################################################################################
-## ON WINDOWS
-
