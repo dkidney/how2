fbdab7af322fdc732c9479bf7528cd5e1c33945c
dkidney <darrenkidney@yahoo.co.uk>
2019-08-03 14:18:54 +0100

    updated website

--- _knitr_config.R
+++ _knitr_config.R
@@ -24,3 +24,14 @@ cat_file = function(x){
 }
 # cache -----
 knitr::opts_chunk$set(cache = TRUE)
+# python -----
+knitr::opts_chunk$set(
+  eval = FALSE,
+  collapse = TRUE,
+    engine.path = list(
+        python = "/usr/local/anaconda3/envs/py3/bin/python"
+    ),
+    python.reticulate = TRUE
+)
+stopifnot(file.exists(knitr::opts_chunk$get("engine.path")$python))
+reticulate::use_python(knitr::opts_chunk$get("engine.path")$python)
