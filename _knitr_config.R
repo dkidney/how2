aeb55808714c4e387a00e3a8d9e1acc996f1f8c0
dkidney <darrenkidney@yahoo.co.uk>
2019-08-20 09:17:24 +0100

    rebuild + new placeholder sections

--- _knitr_config.R
+++ _knitr_config.R
@@ -6,20 +6,46 @@ knitr::opts_chunk$set(
 knitr::opts_chunk$set(
     echo = TRUE,
     results = 'hold',
-    comment = "##",
-    collapse = TRUE,
-    prompt = FALSE,
+    collapse = FALSE,
     warning = FALSE,
     message = FALSE
 )
+# code decoration -----
+knitr::opts_chunk$set(
+    tidy = "styler" ,
+    comment = "# ",
+    background = '#F7F7F7'
+)
+# plots -----
+golden_ratio = (1 + sqrt(5)) / 2
+knitr::opts_chunk$set(
+    fig.show = "hold",
+    dev = "svg", # svg looks better on web pages
+    dpi = 96, # dpi * inches = pixels
+    fig.width = 7,
+    fig.asp = 2 / (1 + sqrt(5)), # fig.height = fig.width * fig.asp
+    out.width = "75%",
+    fig.retina = 1,
+    fig.align = "center"
+)
+# python -----
+knitr::opts_chunk$set(
+  collapse = TRUE,
+    engine.path = list(
+        python = "/usr/local/anaconda3/envs/py3/bin/python"
+    ),
+    python.reticulate = TRUE
+)
+stopifnot(file.exists(knitr::opts_chunk$get("engine.path")$python))
+reticulate::use_python(knitr::opts_chunk$get("engine.path")$python)
 # option templates -----
 # cat_file
 knitr::opts_template$set(
   cat_file = list(
-    eval = TRUE, 
-    echo = FALSE, 
+    eval = TRUE,
+    echo = FALSE,
     results = "asis",
-    collapse = FALSE, 
+    collapse = FALSE,
     comment = ""
   )
 )
@@ -38,13 +64,3 @@ cat_text = function(x, ext = NULL){
 }
 # cache -----
 knitr::opts_chunk$set(cache = TRUE)
-# python -----
-knitr::opts_chunk$set(
-  collapse = TRUE,
-    engine.path = list(
-        python = "/usr/local/anaconda3/envs/py3/bin/python"
-    ),
-    python.reticulate = TRUE
-)
-stopifnot(file.exists(knitr::opts_chunk$get("engine.path")$python))
-reticulate::use_python(knitr::opts_chunk$get("engine.path")$python)
